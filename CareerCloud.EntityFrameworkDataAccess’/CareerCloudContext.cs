48d8fbdea7ec90750c274d7f1d45d975d49c1100
nisethi85 <nisethi85@gmail.com>
2020-02-01 21:46:59 -0500

    Added Assignment4

---
+++ CareerCloud.EntityFrameworkDataAccess’/CareerCloudContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using CareerCloud.Pocos;
+
+class CareerCloudContext : DbContext
+{
+    public DbSet<ApplicantEducationPoco> ApplicantEducations { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder();
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+        config.AddJsonFile(path, false);
+        var root = config.Build();
+       string  _connStr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        optionsBuilder.UseSqlServer("_connStr");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+
+
+}
---
+++ CareerCloud.EntityFrameworkDataAccess’/EFGenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using CareerCloud.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CareerCloud.EntityFrameworkDataAccess_
+{
+    class EFGenericRepository<T> : IDataRepository<T> where T : class
+
+    {
+        private CareerCloudContext _context;
+
+        public EFGenericRepository()
+        {
+            _context = new CareerCloudContext(); 
+
+        }
+        
+        public void Add(params T[] items)
+        {
+            foreach (T item in items)
+                {
+                _context.Entry(item).State=
+                    EntityState.Added;
+            }
+            _context.SaveChanges();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetList(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params T[] items)
+        {
+            foreach(T item in items)
+            {
+                _context.Entry(item).State = EntityState.Deleted;
+            }
+            _context.SaveChanges();
+        }
+
+        public void Update(params T[] items)
+        {
+            foreach (T item in items)
+            {
+                _context.Entry(item).State = EntityState.Modified;
+            }
+            _context.SaveChanges();
+        }
+    }
+}
